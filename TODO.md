1d2fe45408339e423fe09d3d5583353828eb1138
Ben Orchard <thefirstmuffinman@gmail.com>
2022-05-31 13:28:27 +0100

    bleh

---
+++ TODO.md
@@ -0,0 +1,3 @@
+  * split machine and idealized things somehow, so we don't have to do as much
+    unnecessary unboxing. perhaps a class with an associated type family, like
+    in bytepatch?
--- src/FortranSrc/Repr/Value/Scalar/Logical.hs
+++ src/FortranSrc/Repr/Value/Scalar/Logical.hs
@@ -10,16 +10,25 @@ data SomeFLogical pr =
 deriving stock instance Show (SomeFLogical pr)
 instance Eq (SomeFLogical pr) where
     (SomeFLogical _ (FLogicalI b1)) == (SomeFLogical _ (FLogicalI b2)) = b1 == b2
-    (SomeFLogical k1 (FLogicalM a1)) == (SomeFLogical k2 (FLogicalM a2)) =
-        someFIntMBinOp (==) (SomeFInt k1 a1) (SomeFInt k2 a2)
+    (SomeFLogical k1 (FLogicalM i1)) == (SomeFLogical k2 (FLogicalM i2)) =
+        someFIntMBinOp (==) (SomeFInt k1 (FIntM i1)) (SomeFInt k2 (FIntM i2))
 instance Ord (SomeFLogical pr) where
     compare (SomeFLogical _ (FLogicalI b1)) (SomeFLogical _ (FLogicalI b2)) = compare b1 b2
-    compare (SomeFLogical k1 (FLogicalM a1)) (SomeFLogical k2 (FLogicalM a2)) =
-        someFIntMBinOp compare (SomeFInt k1 a1) (SomeFInt k2 a2)
+    compare (SomeFLogical k1 (FLogicalM i1)) (SomeFLogical k2 (FLogicalM i2)) =
+        someFIntMBinOp compare (SomeFInt k1 (FIntM i1)) (SomeFInt k2 (FIntM i2))
 
 data FLogical (pr :: PrimRepr) (k :: FTInt) where
-    FLogicalM :: FInt 'Machine k -> FLogical 'Machine k
+    FLogicalM :: (rep ~ FIntMRep k, Integral rep, Show rep) => rep -> FLogical 'Machine k
     FLogicalI :: Bool -> FLogical 'Idealized k
 deriving stock instance Show (FLogical pr k)
 deriving stock instance Eq   (FLogical pr k)
 deriving stock instance Ord  (FLogical pr k)
+
+someFLogicalNot :: SomeFLogical pr -> SomeFLogical pr
+someFLogicalNot (SomeFLogical k b) =
+    case b of
+      FLogicalM i  ->
+        case i of
+          1 -> SomeFLogical k $ FLogicalM 0
+          _ -> SomeFLogical k $ FLogicalM 1
+      FLogicalI b' -> SomeFLogical k $ FLogicalI $ not b'
